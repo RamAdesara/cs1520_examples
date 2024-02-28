ef53c066f4e48864ea54713ea0d65214df128f50
Ram Adesara <RKA22@pitt.edu>
2024-02-28 12:10:17 -0500

    Modified practice1.js

--- my_practice/practice1.js
+++ my_practice/practice1.js
@@ -1,20 +1,3 @@
-// function test() {
-//     return this.a;
-// }
-
-// var f = test.bind({a: "foo"});
-// var b = f.bind({a: "bar"});
-// var o = {a: 42, test: test, f: f, b: b};
-// console.log(o.a, o.test(), o.f(), o.b());
-
-
-var an_obj = {bar: function() {
-                var x = (() => this);
-                return x;
-            }
-        };
-
-var fn = an_obj.bar();
-console.log(fn());     // ???
-var fn2 = an_obj.bar;
-console.log(fn2()());  // ???
+let arr = [0, 1, 2, 3];
+arr.length = 7;
+console.log(arr[4]);
